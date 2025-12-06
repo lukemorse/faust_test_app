3273c2f054a8738bb5ad43f64a276a32a2bbccdc
Luke Morse <luke.morse@gmail.com>
2025-12-06 14:10:00 -0600

    Guard deprecated iOS audio driver in generated header

--- ios/Faust/ios-faust.h
+++ ios/Faust/ios-faust.h
@@ -1,1 +1,12 @@
 
+// Legacy CoreAudio driver (uses deprecated AudioSession APIs). Disabled by
+// default for modern SDK compatibility. Define FAUST_ENABLE_IOSAUDIO to 1
+// before including this header if you need the CoreAudio hosting helpers.
+#ifndef FAUST_ENABLE_IOSAUDIO
+#define FAUST_ENABLE_IOSAUDIO 0
+#endif
+
+#if FAUST_ENABLE_IOSAUDIO
+
+#endif // FAUST_ENABLE_IOSAUDIO
+
